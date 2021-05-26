920956d15afcecd491adc0e8d5dc43b4a0a40a04
OverZerom <overzerom@gmail.com>
2021-05-26 13:24:57 +0300

    включил автоплей

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     var scroll = new Splide(".splide", {
-        autoplay: false,
+        autoplay: true,
         interval: 6000,
         type: "loop",
         pagination: true,
